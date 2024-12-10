4943684fb8d69097b3deb1fd4363ba3030a2918a
khalid-nawaz11 <khalidmaitla789@gmail.com>
2024-12-11 00:48:55 +0500

    deployment

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const callRoutes = require("./src/routes/callRoutes/callRoutes");
 const customerAuthenticationRoutes = require("./src/routes/AuthenticationRoutes/customerAuthenticationRoutes");
 const paymentRoutes = require("./src/routes/paymentRoutes");
 
+const fs = require("fs"); // Import FileSystem for SSL certificates
+const https = require("https"); // Import HTTPS module
+
+dotenv.config(); // Load environment variables
+
 const app = express();
 app.use(cookieParser());
 app.use(
@@ -31,8 +36,6 @@ app.get("/api/", (req, res) => {
   res.send("Hello World!");
 });
 
-dotenv.config();
-
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
@@ -42,8 +45,22 @@ app.use("/api/customer", customerAuthenticationRoutes);
 app.use("/api/call", callRoutes);
 app.use("/api/payments", paymentRoutes);
 
+// Load SSL Certificates
+const privateKey = fs.readFileSync("private.key", "utf8");
+const certificate = fs.readFileSync("certificate.crt", "utf8");
+
+const credentials = { key: privateKey, cert: certificate };
+
 const IP = process.env.IP || "localhost";
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, IP, () => {
-  console.log(`Server is running on http://${IP}:${PORT}`);
+const HTTP_PORT = process.env.PORT || 3000;
+const HTTPS_PORT = process.env.HTTPS_PORT || 443;
+
+// Start HTTP server
+app.listen(HTTP_PORT, IP, () => {
+  console.log(`HTTP Server is running on http://${IP}:${HTTP_PORT}`);
+});
+
+// Start HTTPS server
+https.createServer(credentials, app).listen(HTTPS_PORT, IP, () => {
+  console.log(`HTTPS Server is running on https://${IP}:${HTTPS_PORT}`);
 });
